1fcbf0e95596c77281819dca63c189e097b64a22
yilingchenn <47489435+yilingchenn@users.noreply.github.com>
2019-02-17 01:57:10 -0800

    Update home_screen.js

--- home_screen.js
+++ home_screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default class SlackIcon extends React.Component {
   render() {
@@ -7,17 +7,21 @@ export default class SlackIcon extends React.Component {
       <View style = {styles.container}>
         <View style = {styles.newcontainer}>
           <Text style={{color: '#BCA18D',fontSize: 25}}> Please Press to Record </Text>
+          <TouchableOpacity onPress={() => console.log('test')}>
             <Image
               source={require('../assets/images/icon-record2.png')}
               fadeDuration={0}
               style={{width: 300, height: 300}}
             />
+          </TouchableOpacity>
           <View style = {styles.anothercontainer}>
+          <TouchableOpacity>
             <Image
               source={require('../assets/images/gps-sign.png')}
               fadeDuration={0}
               style={{width: 175, height: 175}}
             />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
